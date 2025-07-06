c70a6e50cd8d96ac3be4af82637bbb0edf51d0dc
Al Zaplin <zaplin.dev@gmail.com>
2025-07-06 19:43:14 +0300

    Copy comments & clean up

--- deep-merge/test/shallowMerge.test.ts
+++ deep-merge/test/shallowMerge.test.ts
@@ -1,8 +1,6 @@
 import { expect, test } from 'vitest';
 import { deepMerge } from '../src';
 
-// 3.3 Чтобы произвести TDD мы сперва определим что мы ожидаем от функции deepMerge. Для начала рассмотрим простой случай, где мы произведём слияние двух объектов поверхностным образом.
-// Go to [deep-merge\src\index.ts]
 test('shallow merge', () => {
   const merged = deepMerge(
     {
@@ -19,7 +17,6 @@ test('shallow merge', () => {
   });
 });
 
-// 3.5 Отлично, тест прошёл, но теперь рассмотрим ещё случай, где свойства нахлёстываются друг на друга. И мы ожидаем, что значение свойства "github" второго объекта попадёт в итоговый объект.
 test('shallow merge with props overlaps', () => {
   const merged = deepMerge(
     {
@@ -39,8 +36,6 @@ test('shallow merge with props overlaps', () => {
   });
 });
 
-// 3.6 Создадим ещё один тест уже с массивами. Получаем ошибку, т.к. второй массив перезаписал первый, вместо того, чтобы произвести слияние содержимого обоих массивов.
-// Go to [deep-merge\src\index.ts]
 test('shallow merge with arrays', () => {
   const merged = deepMerge(['vue', 'svelte'], ['react', 'angular', 'solid']);
 
